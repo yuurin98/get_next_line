c91c102dd6ed3ff953b88a21e1da478f8730f663
Lim Chee Tian <lchee-ti@u82z01s03.42kl.edu.my>
2023-11-08 16:44:56 +0800

    Starting off

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:16:35 by lchee-ti          #+#    #+#             */
+/*   Updated: 2023/11/08 13:21:19 by lchee-ti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
